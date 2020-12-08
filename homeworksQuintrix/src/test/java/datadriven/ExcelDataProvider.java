d0639752f5013d1faaf0f91d7810935836288cab
Samanja Cartagena <cart.samanja09@gmail.com>
2020-12-08 16:13:05 -0500

    dataprovider

--- homeworksQuintrix/src/test/java/datadriven/ExcelDataProvider.java
+++ homeworksQuintrix/src/test/java/datadriven/ExcelDataProvider.java
@@ -1,11 +1,8 @@
 package datadriven;
-/**
+
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -34,4 +31,3 @@ public class ExcelDataProvider {
 		return data;
 	}
 }
-**/
